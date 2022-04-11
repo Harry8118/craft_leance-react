26df8856e73642a6bb4af85c49b0d13bd07fba7e
Harry <harutsargsyan46@gmail.com>
2022-04-11 15:14:05 +0400

    test

--- src/components/organisms/header/Header.jsx
+++ src/components/organisms/header/Header.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import Links from "../../atoms/link/Links";
 import HeaderContact from "../../molecules/headercontact/HeaderContact";
+import HeaderMenu from "../../molecules/headermenu/HeaderMenu";
 import "./header.scss";
 
 const Header = () => {
   return (
-    <div className="header_top-black container">
-      <Links />
-      <HeaderContact />
-    </div>
+    <header>
+      <div className="container">
+        <div className="header_top-black">
+          <Links />
+          <HeaderContact />
+        </div>
+        <HeaderMenu />
+      </div>
+    </header>
   );
 };
 
--- src/components/template/Page.jsx
+++ src/components/template/Page.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import Header from "../organisms/header/Header";
-import HeaderMenu from "../molecules/headermenu/HeaderMenu";
 
 import "./page.scss";
 
 const Page = () => {
   return (
-    <header>
+    <>
       <Header />
-      <HeaderMenu />
-    </header>
+    </>
   );
 };
 
